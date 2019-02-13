3cd185038d7fc30bbe63f7069276313604c9f0ac
Jacopo <jacopo.slaghenauffi@gmail.com>
2019-02-13 14:28:00 +0100

    multimodal support for loadImage function

--- Functions/loadImage.m
+++ Functions/loadImage.m
@@ -1,7 +1,16 @@
-function [out] = loadImage(imgName)
-% This function takes a filename as input and
-% reads the corresponding image from the dataset
+function [out] = loadImage(id)
+% multimodal function for reading dataset images
+% reads image from either image id or filename
 path="~/Desktop/parsingDataset/RSICD_images/";
-out=imread(path+imgName);
+%% If the identifier is a number, it is the file id in the dataset
+if isa(id,'double')
+    caps=readCaptions();
+    name=caps.images(id).filename;
+    out=imread(path+name);
+end
+%% If the identifier is a string, it is the image filename
+if isa(id,'string') || isa(id,'char')
+    out=imread(path+id);
+end
 end
 

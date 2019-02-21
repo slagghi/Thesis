38e4f4eebddda38c912718b6b702f5745ded6e05
Jacopo <jacopo.slaghenauffi@gmail.com>
2019-02-21 11:31:41 +0100

    Create test network to automatically generate sonnets

---
+++ LSTM/generateText.m
@@ -0,0 +1,31 @@
+% This script generates a random character sequence
+initialCharacters=extractBefore(textData,2);
+firstCharacter=datasample(initialCharacters,1);
+generatedText=string(firstCharacter);
+X=zeros(numUniqueCharacters,1);
+idx=strfind(uniqueCharacters,firstCharacter);
+X(idx)=1;
+vocabulary=string(net.Layers(end).Classes);
+
+maxLength = 500;
+while strlength(generatedText) < maxLength
+    % Predict the next character scores.
+    [net,characterScores] = predictAndUpdateState(net,X,'ExecutionEnvironment','cpu');
+    
+    % Sample the next character.
+    newCharacter = datasample(vocabulary,1,'Weights',characterScores);
+    
+    % Stop predicting at the end of text.
+    if newCharacter == endOfTextCharacter
+        break
+    end
+    
+    % Add the character to the generated text.
+    generatedText = generatedText + newCharacter;
+    
+    % Create a new vector for the next input.
+    X(:) = 0;
+    idx = strfind(uniqueCharacters,newCharacter);
+    X(idx) = 1;
+end
+generatedText = replace(generatedText,[newlineCharacter whitespaceCharacter],[newline " "])
